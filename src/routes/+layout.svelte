3ee9414629e261716d8f3e981406cd8d5ed27929
bmaggiano <brandon.maggiano@gmail.com>
2023-07-11 18:08:57 -0700

    initial file structure for svelte, added nav bar bones, added about, projects, and home page. Added header element with basic color structure of light blue and orange

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<style>
+    h1 {
+        font-family: sans-serif;
+    }
+    .navContainer {
+        display: flex;
+        align-items: center;
+    }
+    nav {
+        padding-left: 2rem;
+    }
+    a {
+        font-family: sans-serif;
+        padding-left: 2rem;
+        text-decoration: none;
+        color: rgb(255, 60, 0);
+        letter-spacing: .2rem;
+        font-weight: bold;
+    }
+    img {
+        height: 3rem;
+        border-radius: 5rem;
+        margin-right: 1rem;
+    }
+</style>
+
+<div class="navContainer">
+        <img
+            src="./smaller.jpg"
+            alt="brandon maggiano"
+            class="headshot"
+            />
+        <h1>My Portfolio</h1>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+    </nav>
+</div>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<style>
+    h1, p {
+        color: black;
+        font-family: sans-serif;
+    }
+    span {
+        font-family: sans-serif;
+        font-style: italic;
+        color: rgb(104, 104, 104);
+    }
+    .header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+        background-color: rgb(203, 242, 255);
+    }
+    .headshot {
+        height: 15rem;
+    }
+    .right-side {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        margin: 2rem;
+    }
+    .title {
+        padding-left: 2rem;
+    }
+</style>
+
+<div class="header">
+    <div class="left-side">
+        <!-- <h1>Brandon Maggiano</h1>
+        <p>Full Stack Software Engineer</p>
+        <span>Chandler, AZ</span> -->
+    </div>
+    <div class="right-side">
+        <img
+        class="headshot"
+        src="./sveltelogo.png"
+        alt="Brandon Maggiano"
+        />
+        <div class="title">
+            <h1>Built using the power of <br/> Svelte</h1>
+            <span>a fast growing font end framework</span>
+        </div>
+    </div>
+</div>
