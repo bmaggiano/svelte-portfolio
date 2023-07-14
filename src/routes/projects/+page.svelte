520b644202da0dd5d5b93a33a48fb809d5b82ebb
bmaggiano <brandon.maggiano@gmail.com>
2023-07-14 12:55:13 -0700

    added basic layout for projects page and added 2 projects to my array of projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,42 @@
-<h1>Projects</h1>
+<script>
+    import { Blockquote } from 'flowbite-svelte'
+    import '../../styles.css'
+
+    const projects = [
+        {
+            name: "Super-ShowDown",
+            tech: "NextJs/Clerk/Prisma/OpenAI",
+            github: "https://github.com/bmaggiano/Super-Showdown",
+            deployedLink:"https://super-showdown.vercel.app/",
+        },
+        {
+            name: "IG Caption Generator",
+            tech: "NextJs/OpenAI/Replicate/UploadThing",
+            github: "https://github.com/bmaggiano/instagram-caption-generator",
+            deployedLink:"https://igcap.dev",
+        },
+    ]
+
+  </script>
+
+
+<div class="header">
+    <div class="right-side">
+        <div class="title">
+            <h1>My Projects</h1><br/>
+              
+              <Blockquote size='xl'>
+              My journey started on 4/21/2022 when I wrote my very <br/> first line of code... "Hello World"
+              </Blockquote>
+        </div>
+        <img
+        class="headshot"
+        src="./sveltelogo.png"
+        alt="Brandon Maggiano"
+        />
+    </div>
+</div>
+
+<div class="subheader">
+    <h2> Making a career change can be one of the most daunting desicions of a person's life...</h2>
+</div>
